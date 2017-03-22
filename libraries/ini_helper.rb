8628a5b23e035a9ae6a15b9d992916f9c0b17f7b
Ramez Hanna <r.hanna@criteo.com>
2017-03-22 15:38:49 +0100

    loop over keys instead of a counter

--- libraries/ini_helper.rb
+++ libraries/ini_helper.rb
@@ -2,7 +2,7 @@ module GrafanaCookbook
   module IniHelper
     def self.format_multi_config(multi_config)
       output = []
-      multi_config.keys do |conf|
+      multi_config.keys.each do |conf|
         output << format_config(multi_config[conf])
       end
       output.join "\n"
